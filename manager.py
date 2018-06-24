6fcef9de737dfc3b4f201de9c16d8789a0265147
小吴 <765645652@qq.com>
2018-06-24 11:08:56 +0800

    script命令扩展

--- manager.py
+++ manager.py
@@ -1,10 +1,13 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
 
 app = Flask(__name__)
 
 
-db = SQLAlchemy(app
+db = SQLAlchemy(app)
+
+manager = Manager(app)
 
 
 a = 10
